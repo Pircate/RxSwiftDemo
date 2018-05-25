198414a63779e0c2ea1a6bb5979df37a1d4ba77c
Pircate <gao497868860@163.com>
2018-05-26 01:10:05 +0800

    remove useless code

--- LightCloud/Classes/Home/View/Controller/HomeViewController.swift
+++ LightCloud/Classes/Home/View/Controller/HomeViewController.swift
@@ -38,7 +38,9 @@ final class HomeViewController: BaseViewController {
         navigation.item.title = "首页"
         navigation.bar.tintColor = UIColor.white
         navigation.item.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: nil, action: nil)
-        navigation.item.leftBarButtonItem?.rx.tap.bind(to: rx.push(to: QueryViewController())).disposed(by: disposeBag)
+        navigation.item.leftBarButtonItem?.rx.tap.subscribe({ [weak self] _ in
+            self?.navigationController?.pushViewController(QueryViewController(), animated: true)
+        }).disposed(by: disposeBag)
         navigation.item.rightBarButtonItem = UIBarButtonItem(title: "登录")
         navigation.item.rightBarButtonItem?.rx.tap.bind(to: rx.gotoLogin).disposed(by: disposeBag)
         
@@ -75,26 +77,25 @@ final class HomeViewController: BaseViewController {
         output.items.map(to: ()).drive(tableView.cr.header!.rx.endRefreshing).disposed(by: disposeBag)
         
         // cell 删除操作
-        tableView.rx.itemDeleted.subscribe(onNext: { [weak self] (indexPath) in
-            guard let `self` = self else { return }
-            var sections = viewModel.dataSource.sectionModels
-            var items = sections[indexPath.section].items
-            let item = items[indexPath.row]
-            item.rx.delete().loading()
+        tableView.rx.itemDeleted.flatMap({ indexPath in
+            viewModel.dataSource[indexPath].rx.delete().loading()
                 .catchErrorJustToast()
                 .showToast(onSuccess: "删除成功")
-                .subscribe(onNext: { _ in
-                    items.remove(at: indexPath.row)
-                    if items.isEmpty {
-                        sections.remove(at: indexPath.section)
-                        viewModel.dataSource.setSections(sections)
-                        self.tableView.deleteSections([indexPath.section], animationStyle: .automatic)
-                    } else {
-                        sections[indexPath.section].items = items
-                        viewModel.dataSource.setSections(sections)
-                        self.tableView.deleteRows(at: [indexPath], with: .automatic)
-                    }
-                }).disposed(by: self.disposeBag)
+                .map({ _ in indexPath })
+        }).subscribe(onNext: { [weak self] indexPath in
+            var sections = viewModel.dataSource.sectionModels
+            var items = sections[indexPath.section].items
+            items.remove(at: indexPath.row)
+            guard let `self` = self else { return }
+            if items.isEmpty {
+                sections.remove(at: indexPath.section)
+                viewModel.dataSource.setSections(sections)
+                self.tableView.deleteSections([indexPath.section], animationStyle: .automatic)
+            } else {
+                sections[indexPath.section].items = items
+                viewModel.dataSource.setSections(sections)
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            }
         }).disposed(by: disposeBag)
         
         // cell 移动操作
--- LightCloud/Extension/UIViewController+Rx.swift
+++ LightCloud/Extension/UIViewController+Rx.swift
@@ -11,12 +11,6 @@ import ExtensionX
 
 extension Reactive where Base: UIViewController {
     
-    func push(to viewController: UIViewController) -> Binder<Void> {
-        return Binder(base) { from, to in
-            from.navigationController?.pushViewController(viewController, animated: true)
-        }
-    }
-    
     var pop: Binder<Void> {
         return Binder(base) { vc, _ in
             vc.navigationController?.popViewController(animated: true)
