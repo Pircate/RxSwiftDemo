7b9856f964533c561f95beb699af4a3e5bd2bb63
Pircate <gao497868860@163.com>
2018-06-04 15:25:40 +0800

    Commit: update countdown

--- Pods/RxExtension/Sources/Swift+Rx/Int+Rx.swift
+++ Pods/RxExtension/Sources/Swift+Rx/Int+Rx.swift
@@ -10,11 +10,11 @@ import RxSwift
 
 public extension Int {
     
-    func countdown(_ resend: String = "重新发送") -> Observable<(title: String, isEnabled: Bool)> {
+    func countdown(_ suffix: String = "秒", resend: String = "重新发送") -> Observable<(title: String, isEnabled: Bool)> {
         return Observable.of(self).flatMap({ (until) -> Observable<(title: String, isEnabled: Bool)> in
             Observable<Int>.timer(0, period: 1, scheduler: MainScheduler.instance).map({
                 if until > $0 {
-                    return ("\(until - $0)s", false)
+                    return ("\(until - $0)\(suffix)", false)
                 }
                 throw NSError()
             }).catchErrorJustReturn((resend, true))
