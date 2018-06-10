bd31d8a0b6902c96e2570c87edb543776d5ac795
Pircate <gao497868860@163.com>
2018-06-10 13:20:42 +0800

    update navigation bar

--- LightCloud/App/AppDelegate+Root.swift
+++ LightCloud/App/AppDelegate+Root.swift
@@ -13,7 +13,7 @@ extension AppDelegate {
         window = UIWindow(frame: UIScreen.main.bounds).chain.backgroundColor(UIColor.white).build
         let nav = UINavigationController(rootViewController: MainViewController())
         nav.navigation.configuration.isEnabled = true
-        nav.navigation.configuration.isTranslucent = true
+        nav.navigation.configuration.isTranslucent = false
         nav.navigation.configuration.barTintColor = UIColor(hex: "#4381E8")
         nav.navigation.configuration.titleTextAttributes = [.foregroundColor: UIColor.white]
         nav.navigationBar.barStyle = .black
